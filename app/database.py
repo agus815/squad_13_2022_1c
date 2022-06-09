d1b7d190c107d48ea33fa8397292135be52b4ca5
enrique.morici <enrique.morici@daitek.ar>
2022-06-09 20:48:54 -0300

    Estructura básica del proyecto. Integración DB. Condig inicial

---
+++ app/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from .config import settings
+
+SQLALCHEMY_DATABASE_URL = "postgresql://{}:{}@{}/{}".format(settings.database_user,settings.database_password, settings.database_ip, settings.database_name)
+print(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={
+        "keepalives": 1,
+        "keepalives_idle": 30,
+        "keepalives_interval": 10,
+        "keepalives_count": 5,
+    })
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
---
+++ app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from .config import settings
+
+from .database import SessionLocal
+
+from .database import Base, engine
+
+from .routes import proyecto_routes
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(proyecto_routes.router, prefix="/proyectos", tags=["proyectos"])
