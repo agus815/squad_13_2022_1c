37ce63e94378522f91afc9e9d41674dd491975e0
agus815 <asu@fi.uba.ar>
2022-06-14 04:56:25 -0300

    crud, model, schemas (sin recursos)

--- app/routes/proyecto_routes.py
+++ app/routes/proyecto_routes.py
@@ -11,6 +11,7 @@ from ..schemas.schemas_proyectos import *
 
 router = APIRouter()
 
+
 # Dependency
 def get_db():
     db = SessionLocal()
@@ -19,24 +20,29 @@ def get_db():
     finally:
         db.close()
 
+
 @router.get("/", response_model=List[Proyecto])
 def read_proyectos(db: Session = Depends(get_db)):
     return get_proyectos(db)
+
 
 @router.get("/{codigo}", response_model=Proyecto)
 def read_proyecto(codigo: int, db: Session = Depends(get_db)):
     return get_proyecto(codigo, db)
 
+
 @router.post("/create", response_model=Proyecto)
 def create_proyecto(proyecto: ProyectoCreate, db: Session = Depends(get_db)):
     proyecto_db = get_proyecto_by_nombre(proyecto.nombre, db)
-    if proyecto_db :
+    if proyecto_db:
         raise HTTPException(status_code=400, detail="El proyecto ya estaba registrado")
     return save_proyecto(proyecto, db)
+
 
 @router.post("/update", response_model=Proyecto)
 def update_proyecto_api(proyecto: ProyectoUpdate, db: Session = Depends(get_db)):
     return update_proyecto(proyecto, db)
+
 
 @router.post("/delete")
 def remove_proyecto(proyecto: ProyectoDelete, db: Session = Depends(get_db)):
--- app/schemas/schemas_proyectos.py
+++ app/schemas/schemas_proyectos.py
@@ -2,26 +2,33 @@ from datetime import date
 from typing import List, Optional
 from pydantic import BaseModel, Field
 
-class ProyectoRecurso(BaseModel) :
+
+class ProyectoRecurso(BaseModel):
     codigo_proyecto: int = Field(..., title="Código del proyecto al que el recurso fue asignado")
     legajo_recurso: int = Field(..., title="Número de legajo del recurso asigando")
 
-class ProyectoBase(BaseModel) :
+
+class ProyectoBase(BaseModel):
     nombre: str = Field(..., title="Nombre del proyecto")
     tipo: str = Field(..., title="Tipo del proyecto. Puede ser implementación o desarrollo")
     fecha_limite: date = Field(date.today(), title="Fecha límite para completar el proyecto")
     recursos: Optional[List[int]] = Field([], title="Números de legajo de los recursos asignados")
 
-class ProyectoDelete(BaseModel) : 
+
+class ProyectoDelete(BaseModel):
     codigo: int = Field(..., title="Codigo del proyecto en la DB")
 
-class ProyectoUpdate(ProyectoBase) :
+
+class ProyectoUpdate(ProyectoBase):
     codigo: int = Field(..., title="Codigo del proyecto en la DB")
+
 
 class ProyectoCreate(ProyectoBase):
     pass
 
+
 class Proyecto(ProyectoBase):
     codigo: int = Field(..., title="Codigo del proyecto en la DB")
+
     class Config:
         orm_mode = True
