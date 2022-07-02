1c41eb58cd7b28f6e4cb96e12b05b174f5507970
agus815 <asu@fi.uba.ar>
2022-07-02 20:24:53 -0300

    BDD para tareas

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -2,8 +2,6 @@ from behave import *
 import requests
 from bdd_context import BDD_Context
 
-exception = []
-
 
 @given('No hay proyecto llamado "{nombre_proyecto}"')
 def delete_proyecto_if_exists(context, nombre_proyecto):
@@ -25,7 +23,7 @@ def create_proyecto(context, nombre_proyecto):
             "fecha_limite": '2022-09-02',
             "tareas": []})
         response.raise_for_status()
-        BDD_Context.save_proyect_code(context, response.json())
+        BDD_Context.save_proyect_code(context, response.json()["codigo"])
         BDD_Context.save_response_code(context, response)
     except requests.exceptions.HTTPError as err:
         BDD_Context.save_response_message(context, err.response.json()["detail"])
@@ -53,7 +51,7 @@ def create_proyecto_if_not_exists(context, nombre_proyecto):
     for proyecto in response.json():
         if proyecto["nombre"] == nombre_proyecto:
             existe_proyecto = True
-            BDD_Context.save_proyect_code(context, proyecto)
+            BDD_Context.save_proyect_code(context, proyecto["codigo"])
     if not existe_proyecto:
         create_proyecto(context, nombre_proyecto)
 
@@ -129,3 +127,128 @@ def lista_no_contiene_proyecto(context, nombre_proyecto):
         if proyecto["nombre"] == nombre_proyecto:
             contiene_proyecto = True
     assert (not contiene_proyecto)
+
+
+@when('Se trata de crear una tarea llamada "{nombre_tarea}"')
+def crear_tarea(context, nombre_tarea):
+    try:
+        response = requests.post('https://backend-proyectos-g13.herokuapp.com/tareas/create', json={
+            "codigo_proyecto": context.proyect_code,
+            "nombre": nombre_tarea,
+            "descripcion": "",
+            "estado": "Creada",
+            "duracion": 7,
+            "prioridad": "Baja",
+            "fecha_inicio": "2022-07-02",
+            "fecha_fin": "2022-10-02",
+            "recurso": 0})
+        response.raise_for_status()
+        BDD_Context.save_proyect_code(context, response.json()["codigo_proyecto"])
+        BDD_Context.save_task_code(context, response.json()["codigo"])
+        BDD_Context.save_response_code(context, response)
+    except requests.exceptions.HTTPError as err:
+        BDD_Context.save_response_message(context, err.response.json()["detail"])
+
+
+@then('La lista de proyectos debera contener "{nombre_tarea}"')
+def proyecto_contiene_tarea(context, nombre_tarea):
+    existe_tarea = False
+    ruta = 'https://backend-proyectos-g13.herokuapp.com/tareas/proyecto/' + str(context.proyect_code)
+    response = requests.get(ruta)
+    for tarea in response.json():
+        if (tarea["nombre"] == nombre_tarea) and (tarea["codigo"] == context.task_code):
+            existe_tarea = True
+    assert existe_tarea
+
+
+@given('Un proyecto que no existe')
+def set_proyecto_que_no_existe(context):
+    BDD_Context.save_proyect_code(context, 9999)
+
+
+@when('Se trata de crear una tarea asociada al proyecto')
+def crear_tarea_con_proyecto_invalido(context):
+    crear_tarea(context, "Tarea que no debe crearse")
+
+
+@given('Existe una tarea llamada "{nombre_tarea}"')
+def crear_tarea_si_no_existe(context, nombre_tarea):
+    response = requests.get('https://backend-proyectos-g13.herokuapp.com/tareas/')
+    for tarea in response.json():
+        if tarea["nombre"] == nombre_tarea:
+            BDD_Context.save_proyect_code(context, tarea["codigo_proyecto"])
+            BDD_Context.save_task_code(context, tarea["codigo"])
+            return
+    crear_tarea(context, nombre_tarea)
+
+
+@given('El estado de la tarea es "{estado}"')
+def set_tarea_state(context, estado):
+    ruta = 'https://backend-proyectos-g13.herokuapp.com/tareas/' + str(context.task_code)
+    response = requests.get(ruta)
+    respuesta = response.json()
+    respuesta["estado"] = estado
+    requests.post('https://backend-proyectos-g13.herokuapp.com/tareas/update', json=respuesta)
+
+
+@when('Se trata de modificar el {campo} de la tarea a "{nuevo_campo}"')
+def modify_task(context, campo, nuevo_campo):
+    ruta = 'https://backend-proyectos-g13.herokuapp.com/tareas/' + str(context.task_code)
+    response = requests.get(ruta)
+    respuesta = response.json()
+    respuesta[campo] = nuevo_campo
+    try:
+        response = requests.post('https://backend-proyectos-g13.herokuapp.com/tareas/update', json=respuesta)
+        response.raise_for_status()
+        BDD_Context.save_response_code(context, response)
+    except requests.exceptions.HTTPError as err:
+        BDD_Context.save_response_message(context, err.response.json()["detail"])
+
+
+@then('El estado de la tarea sera "{nuevo_estado}"')
+def assert_task_state(context, nuevo_estado):
+    ruta = 'https://backend-proyectos-g13.herokuapp.com/tareas/' + str(context.task_code)
+    response = requests.get(ruta)
+    assert (response.json()["estado"] == nuevo_estado)
+
+
+@given('Una tarea que no existe')
+def set_tarea_que_no_existe(context):
+    BDD_Context.save_task_code(context, 9999)
+
+
+@when('Se trata de modificar el estado de la tarea que no existe a "{nuevo_estado}"')
+def modificar_tarea_que_no_existe(context, nuevo_estado):
+    try:
+        response = requests.post('https://backend-proyectos-g13.herokuapp.com/tareas/update', json={
+            "codigo_proyecto": 30,
+            "nombre": "Tarea que no debe existir",
+            "descripcion": "",
+            "estado": nuevo_estado,
+            "duracion": 8,
+            "prioridad": "string",
+            "fecha_inicio": "2022-09-02",
+            "fecha_fin": "2022-10-02",
+            "recurso": 0,
+            "codigo": context.task_code
+        })
+        response.raise_for_status()
+        BDD_Context.save_response_code(context, response)
+    except requests.exceptions.HTTPError as err:
+        BDD_Context.save_response_message(context, err.response.json()["detail"])
+
+
+@when('Se trata de eliminar la tarea')
+def eliminar_tarea(context):
+    response = requests.post('https://backend-proyectos-g13.herokuapp.com/tareas/delete', json={'codigo': context.task_code})
+    BDD_Context.save_response_code(context, response)
+
+
+@then('La lista de tareas no contiene la tarea')
+def asset_tareas_no_contiene_tarea(context):
+    contiene_tarea = False
+    response = requests.get('https://backend-proyectos-g13.herokuapp.com/tareas/')
+    for tarea in response.json():
+        if tarea["codigo"] == context.task_code:
+            contiene_tarea = True
+    assert (not contiene_tarea)
---
+++ features/tareas.feature
@@ -0,0 +1,33 @@
+Feature: Gestion de Tarea
+
+  #Escenarios relacionados con la creacion de tareas
+  Scenario: Crear tarea exitosamente
+    Given Existe un proyecto llamado "Proyecto PSA"
+    When Se trata de crear una tarea llamada "Nueva Tarea"
+    Then La solicitud sera exitosa
+    And La lista de proyectos debera contener "Nueva Tarea"
+
+  Scenario: No se puede crear una tarea de un proyecto que no existe
+    Given Un proyecto que no existe
+    When Se trata de crear una tarea asociada al proyecto
+    Then La accion debera ser negada porque "El proyecto no existe"
+
+  #Escenarios relacionados con la modificacion de tareas
+  Scenario: Modificar campos de la tarea exitosamente
+    Given Existe una tarea llamada "Nueva Tarea"
+    And El estado de la tarea es "Creada"
+    When Se trata de modificar el estado de la tarea a "En desarrollo"
+    Then La solicitud sera exitosa
+    And El estado de la tarea sera "En desarrollo"
+
+  Scenario: No se puede modificar campos de una tarea que no existe
+    Given Una tarea que no existe
+    When Se trata de modificar el estado de la tarea que no existe a "En desarrollo"
+    Then La accion debera ser negada porque "La tarea no existe"
+
+
+  #Escenarios relacionados con la eliminacion de tareas
+  Scenario: Eliminar tarea exitosamente
+    Given Existe una tarea llamada "Nueva Tarea"
+    When Se trata de eliminar la tarea
+    Then La lista de tareas no contiene la tarea
