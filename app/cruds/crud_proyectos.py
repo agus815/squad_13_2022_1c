11a2d159d7ba4a3a8a7da33c03378d678b9d5520
agus815 <asu@fi.uba.ar>
2022-07-04 15:44:30 -0300

    Fix bug cuando se eliminan tareas

--- app/cruds/crud_proyectos.py
+++ app/cruds/crud_proyectos.py
@@ -53,11 +53,13 @@ def save_proyecto(proyecto: ProyectoCreate, db: Session) -> Proyecto:
         logger.error("Error al agregar el proyecto: " + str(e))
         raise HTTPException(status_code=500, detail="Problemas al agregar el proyecto")
 
+
 def tarea_generator(proyecto: ProyectoCreate, codigo_proyecto: int):
     for tarea in proyecto.tareas:
         new_tarea = TareaCreate(**tarea.dict())
         new_tarea.codigo_proyecto = codigo_proyecto
         yield new_tarea
+
 
 # ------------------------- UPDATE FUNCTIONS ------------------------------------------
 def update_proyecto(proyecto_new: ProyectoUpdate, db: Session) -> Proyecto:
@@ -74,33 +76,32 @@ def update_proyecto(proyecto_new: ProyectoUpdate, db: Session) -> Proyecto:
         if proyecto_new.tipo != None: proyecto_old.tipo = proyecto_new.tipo
         if proyecto_new.estado != None: proyecto_old.estado = proyecto_new.estado
         if proyecto_new.fecha_limite != None: proyecto_old.fecha_limite = proyecto_new.fecha_limite
-        if len(proyecto_new.tareas) > 0 :
-            old_tareas = get_tareas_from_proyecto(proyecto_new.codigo, db)
-            new_tareas = []
-            for tarea in proyecto_new.tareas :
-                tarea_db = get_tarea(tarea.codigo, db)
-                if tarea_db :
-                    if tarea_db in old_tareas:
-                        tarea_new = TareaUpdate(**tarea.dict())
-                        tarea_updated = update_tarea(tarea_new, db)
-                        new_tareas.append(tarea_updated.codigo)
-                else:
-                    tarea_create = TareaCreate(
-                        codigo_proyecto=tarea.codigo_proyecto,
-                        nombre=tarea.nombre,
-                        descripcion=tarea.descripcion,
-                        estado=tarea.estado,
-                        duracion=tarea.duracion,
-                        prioridad=tarea.prioridad,
-                        fecha_inicio=tarea.fecha_inicio,
-                        fecha_fin=tarea.fecha_fin,
-                        recurso=tarea.recurso
-                    )
-                    tarea_saved = save_tarea(tarea_create, db)
-                    new_tareas.append(tarea_saved.codigo)
-            for tarea in old_tareas :
-                if tarea.codigo not in new_tareas:
-                    delete_tarea(tarea.codigo, db)
+        old_tareas = get_tareas_from_proyecto(proyecto_new.codigo, db)
+        new_tareas = []
+        for tarea in proyecto_new.tareas :
+            tarea_db = get_tarea(tarea.codigo, db)
+            if tarea_db :
+                if tarea_db in old_tareas:
+                    tarea_new = TareaUpdate(**tarea.dict())
+                    tarea_updated = update_tarea(tarea_new, db)
+                    new_tareas.append(tarea_updated.codigo)
+            else:
+                tarea_create = TareaCreate(
+                    codigo_proyecto=tarea.codigo_proyecto,
+                    nombre=tarea.nombre,
+                    descripcion=tarea.descripcion,
+                    estado=tarea.estado,
+                    duracion=tarea.duracion,
+                    prioridad=tarea.prioridad,
+                    fecha_inicio=tarea.fecha_inicio,
+                    fecha_fin=tarea.fecha_fin,
+                    recurso=tarea.recurso
+                )
+                tarea_saved = save_tarea(tarea_create, db)
+                new_tareas.append(tarea_saved.codigo)
+        for tarea in old_tareas :
+            if tarea.codigo not in new_tareas:
+                delete_tarea(tarea.codigo, db)
         db.commit()
         db.refresh(proyecto_old)
         return proyecto_old
